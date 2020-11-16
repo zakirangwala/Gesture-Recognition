35915fdc789939ec89d1c79807a383b89576aefc
Zaki Rangwala <zakirangwala@gmail.com>
2020-11-16 18:47:02 -0500

    Add additional style

--- src/utilities.js
+++ src/utilities.js
@@ -7,6 +7,31 @@ const fingerjoints = {
   pinky: [0, 17, 18, 19, 20],
 };
 
+/* // Infinity Gauntlet Style
+const style = {
+  0: { color: "yellow", size: 15 },
+  1: { color: "gold", size: 6 },
+  2: { color: "green", size: 10 },
+  3: { color: "gold", size: 6 },
+  4: { color: "gold", size: 6 },
+  5: { color: "purple", size: 10 },
+  6: { color: "gold", size: 6 },
+  7: { color: "gold", size: 6 },
+  8: { color: "gold", size: 6 },
+  9: { color: "blue", size: 10 },
+  10: { color: "gold", size: 6 },
+  11: { color: "gold", size: 6 },
+  12: { color: "gold", size: 6 },
+  13: { color: "red", size: 10 },
+  14: { color: "gold", size: 6 },
+  15: { color: "gold", size: 6 },
+  16: { color: "gold", size: 6 },
+  17: { color: "orange", size: 10 },
+  18: { color: "gold", size: 6 },
+  19: { color: "gold", size: 6 },
+  20: { color: "gold", size: 6 },
+}; */
+
 // Drawing Function
 export const drawHand = (predictions, ctx) => {
   // check predictions
@@ -40,6 +65,9 @@ export const drawHand = (predictions, ctx) => {
         ctx.beginPath();
         ctx.arc(x, y, 5, 0, 3 * Math.PI);
         ctx.fillStyle = "indigo";
+        /* //Infinity Gauntlet
+        ctx.arc(x, y, style[i]["size"], 0, 3 * Math.PI);
+        ctx.fillStyle = style[i]["color"]; */
         ctx.fill();
       }
     });
