b845c6660114923d94cf9655ad811ad5d6573606
Zaki Rangwala <zakirangwala@gmail.com>
2020-11-16 07:39:13 -0500

    Construct detect function using loaded tensorflow model

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import * as tf from "@tensorflow/tfjs";
+// import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
 import "./App.css";
@@ -8,6 +8,38 @@ function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
+  const runHandpose = async () => {
+    const net = await handpose.load();
+    console.log("Handpose model loaded");
+    // detect hands
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    // check data
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = video.videoWidth;
+      const videoHeight = video.videoHeight;
+      //video properties
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // canvasRef.current.video.width = videoWidth;
+      // canvasRef.current.video.height = videoHeight;
+      // detections
+      const hand = await net.estimateHands(video);
+      console.log(hand);
+    }
+  };
+
+  runHandpose();
   return (
     <div className="App">
       <header className="App-header">
